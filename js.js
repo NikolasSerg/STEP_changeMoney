c88b5ca4bf56965606ddeedce12cb4a6f27e8ffa
Sergiu Nikolaichuk <Nikolas.ser.a@yandex.ru>
2018-10-15 17:55:41 +0300

    #2chMoney

--- js.js
+++ js.js
@@ -2,10 +2,15 @@ window.onload = function () {
     var url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
 
     Request(url, test);
-
-
 };
-
+ var usdBuy = 0;
+ var usdSale = 0;
+ var eurBuy = 0;
+ var eurSale = 0;
+ var rubBuy = 0;
+ var rubSale = 0;
+ var btcBuy = 0;
+ var btcSale = 0;
 
 function Request(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -14,7 +19,7 @@ function Request(url, callback) {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(xhr.responseText);
-            // console.log(data);
+            console.log(data);
             callback(data);
         }
     };
@@ -22,6 +27,20 @@ function Request(url, callback) {
 }
 
 function test(data){
-    console.log("test" + data[0].ccy);
-    
+    usdBuy = data[0].buy;
+    console.log(usdBuy);
+    usdSale = data[0].sale;
+    console.log(usdSale); 
+    eurBuy = data[1].buy;
+    console.log(eurBuy);
+    eurSale = data[1].sale;
+    console.log(eurSale);
+    rubBuy = data[2].buy;
+    console.log(rubBuy);
+    rubSale = data[2].sale;
+    console.log(rubSale);
+    btcBuy = data[3].buy;
+    console.log(btcBuy)
+    btcSale = data[3].sale;
+    console.log(btcSale)
 }
